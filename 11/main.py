25898cf2cc405c84ccf154f82454510a665072d8
gavlna <kopciljakub@gmail.com>
2024-12-28 15:52:06 +0100

    day 11

---
+++ 11/main.py
@@ -0,0 +1,28 @@
+line = list(map(int, input().split()))
+
+cache = {}
+
+
+def solve(element, iterationsLeft):
+    if iterationsLeft == 0:
+        return 1
+    if (element, iterationsLeft) in cache:
+        return cache[(element, iterationsLeft)]
+    if element == 0:
+        output = solve(1, iterationsLeft - 1)
+    else:
+        string = str(element)
+        if len(string) % 2 == 0:
+            output = solve(int(string[: len(string) // 2]), iterationsLeft - 1) + solve(
+                int(string[len(string) // 2 :]), iterationsLeft - 1
+            )
+        else:
+            output = solve(element * 2024, iterationsLeft - 1)
+    cache[(element, iterationsLeft)] = output
+    return output
+
+
+count = 0
+for x in line:
+    count += solve(x, 25)
+print(count)
